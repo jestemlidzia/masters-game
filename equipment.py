c885b4b4fd6bae164de5769a770b6a429ec2116b
LAPTOP-0HIBJRMG\olesn <olesnik1999@gmail.com>
2023-05-27 15:44:21 +0200

    equip update

--- equipment.py
+++ equipment.py
@@ -6,39 +6,35 @@ import board
 class Equipment(object):
     def __init__(self, screen, level_number):
 
-        # f = open('levels.json')
-        # data = json.load(f)
-        # f.close()
-
         self.screen = screen
-        # level_data = data['Levels'][level_number]
-        # self.level_name = level_data['Name']
-
         self.board = board.Board(self.screen, 0)
 
         self.backpack_view = pygame.image.load(os.path.join("art", 'equip.png'))
         self.active_area_view = pygame.image.load(os.path.join("art", 'active-box.png'))
         self.inactive_area_view = pygame.image.load(os.path.join("art", 'inactive-box.png'))
-        # self.equip_item = pygame.image.load(os.path.join("art", level_data['EquipImage']))
 
         self.screen.blit(self.backpack_view, (384,720))
-        
-        # (384, 650) is visible
+        # (384, 650) cords where backpack is visible
 
-        self.img = []
         self.item_list = []
         self.x_areas_range = [(384,447), (448, 511), (512, 575), (576, 639), (640, 703), (704, 767), (768, 831), (832, 895)]
         self.y_area_range = (650, 710)
+        self.equip_size = 8
+
         self.is_open = False
         self.active_area_range = False
+        self.selected_item = False
+        self.is_selected = False
 
     def add_item_to_equipment(self, item):
-        self.item_list.append(item)
-        # self.img.append(item.name + '-equip.png')
-        print('The item {} has been put into the backpack'.format(item.name))
+        if len(self.item_list) < self.equip_size:
+            self.item_list.append(item)
+            print('The item {} has been put into the backpack'.format(item.name))
+        else:
+            print('Your backpack is full! Press "r" on the backpack item you want to discard to free up some space!')
 
     def print_backpack(self):
-        if len(self.item_list)!=0:
+        if len(self.item_list) != 0:
             print('Items in your backpack:')
             for lifted_item in self.item_list:
                 print(lifted_item.name)
@@ -46,7 +42,6 @@ class Equipment(object):
             print('Your backpack is empty')
 
     def check_item_in_backpack(self, item_name):
-        # if item in self.item_list:
         for i in range(len(self.item_list)):
             if item_name == self.item_list[i].name:
                 print('{} is in the backpack'.format(item_name))
@@ -70,35 +65,37 @@ class Equipment(object):
         self.last_screenn = self.screen.copy()
         for i in range(len(self.x_areas_range)):
             if self.x_areas_range[i][0] <= pygame.mouse.get_pos()[0] <= self.x_areas_range[i][1] and self.y_area_range[0] <= pygame.mouse.get_pos()[1] <= self.y_area_range[1]:
+                self.is_selected = False
                 if self.active_area_range != False:
-                    self.hide_selected_area(self.active_area_range)
+                    self.screen.blit(self.inactive_area_view, (self.active_area_range[0], self.y_area_range[0]))
                 self.screen.blit(self.active_area_view, (self.x_areas_range[i][0], self.y_area_range[0]))
                 self.active_area_range = (self.x_areas_range[i][0], self.x_areas_range[i][1])
                 return self.active_area_range
         return False
-    
-    def hide_selected_area(self, cords):
-        self.screen.blit(self.inactive_area_view, (cords[0], self.y_area_range[0]))
 
     def update_equip(self):
-        # for item in self.item_list:
-        #     self.img.append(item.name + '-equip.png')
-        print('aoaoao', self.board.level_items)
-        print('aoaod', self.item_list)
-        
-        # for item in self.board.level_items:
-        #     print(item.name)
-        #     if item.name == self.item_list[item].name:
-        #         print(item.EquipImage, 'sss')
-        #         self.equip_item = pygame.image.load(os.path.join("art", item.EquipImage))
-        #         self.screen.blit(self.equip_item, (self.x_areas_range[item][0], self.y_area_range[0]+20))
+        area_number = 0
+        for item in self.item_list:
+            if area_number < len(self.x_areas_range):
+                showing_taken_item = pygame.image.load(os.path.join("art", item.equip_img))
+                self.screen.blit(showing_taken_item, (self.x_areas_range[area_number][0]+20,self.y_area_range[0]+20))
+                if self.active_area_range != False:
+                    if self.active_area_range[0] == self.x_areas_range[area_number][0]:
+                        self.selected_item = item
+                    if self.is_selected:
+                        self.selected_item = False
+                area_number = area_number + 1
+            # else:
+            #     print('Your backpack is full! Throw an item out of your backpack to free up some space!')
 
-    def get_image_area(self):
-        return self.game_item.get_rect(topleft = self.position)
-
-    def select_item(self):
-        print('TODO')
+    def get_selected_item(self):
+        if self.selected_item != False:
+            print('The {} was selected'.format(self.selected_item.name))
+            return self.selected_item.name
 
     def remove_item(self):
-        print('TODO')
-    
+        if self.selected_item != False and len(self.item_list) != 0:
+            self.item_list.remove(self.selected_item)
+            self.screen.blit(self.backpack_view, (384,650))
+            self.is_selected = True
+
